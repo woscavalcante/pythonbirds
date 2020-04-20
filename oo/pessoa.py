f6a812d482b34f1adc2a5012f44b3e9c019309df
Willian Cavalcante <woscavalcante@outlook.com>
2020-04-20 19:18:04 -0300

    Criada método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar()) #passa o objeto p implíssito
