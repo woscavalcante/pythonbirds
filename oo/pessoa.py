301628645f7819d7ca4224c32190110275eb7908
Willian Cavalcante <woscavalcante@outlook.com>
2020-04-21 17:49:35 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa: #Pessoa é uma classe
+    olhos = 2 #Atribudo de classe ou instância.
+
     def __init__(self, *filhos, nome=None, idade=39): #isto é um método especial
         self.idade = idade
         self.nome = nome
@@ -19,6 +21,14 @@ if __name__ == '__main__':
         print(filho.nome)
     carolina.sobrenome = 'Costa'
     del carolina.filhos
+    carolina.olhos = 1
+    del carolina.olhos #inserido por último no código às 17:48
     print(carolina.__dict__)
     print(gustavo.__dict__)
+    Pessoa.olhos = 3 #Mutação(alterado) do atributo de classe.
+    print(Pessoa.olhos)
+    print(carolina.olhos)
+    print(gustavo.olhos)
+    print(id(Pessoa.olhos), id(carolina.olhos), id(gustavo.olhos))
+
 
